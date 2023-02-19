814fdea1d997c1c7b0f66e080e3063aad6cc66c7
Jt123 <73838459+Hamsandwich1@users.noreply.github.com>
2023-02-19 17:59:23 +0000

    Add files via upload

---
+++ pygame/graphics/Map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Map" tilewidth="32" tileheight="32" tilecount="11400" columns="114">
+ <image source="../../../Downloads/06 - player animations/6 - player animations/graphics/tilemap/ground.png" width="3648" height="3200"/>
+</tileset>
